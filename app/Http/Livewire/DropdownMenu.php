fba161f545e3ab89cf15f34351eefdbdcc0126f3
*MwfApGRKjs <ak.onodera1230@gmail.com>
2023-08-18 08:26:05 +0900

    お気に入り機能の実装

--- app/Http/Livewire/DropdownMenu.php
+++ app/Http/Livewire/DropdownMenu.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Favorite;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class DropdownMenu extends Component
@@ -9,10 +11,12 @@ class DropdownMenu extends Component
     public $announce;
     public $isVisibleDropdownMenu = false;
     public $isVisibleDeleteModal = false;
+    public $isFavorited;
 
     public function mount($announce)
     {
         $this->announce = $announce;
+        $this->isFavorited = Auth::user()->isFavorite($announce->id);
     }
 
     public function closeDropdownMenu()
@@ -24,6 +28,37 @@ class DropdownMenu extends Component
     {
         $this->isVisibleDropdownMenu = false;
         $this->isVisibleDeleteModal = true;
+    }
+
+    public function favorite()
+    {
+        Favorite::create([
+            'user_id' => Auth::id(),
+            'announce_id' => $this->announce->id,
+        ]);
+    }
+
+    public function unfavorite()
+    {
+        Favorite::where('announce_id', $this->announce->id)
+            ->where('user_id', Auth::id())
+            ->delete();
+    }
+
+    public function toggleFavorite()
+    {
+        if ($this->isFavorited) {
+            Favorite::where('announce_id', $this->announce->id)
+                ->where('user_id', Auth::id())
+                ->delete();
+        } else {
+            Favorite::create([
+                'user_id' => Auth::id(),
+                'announce_id' => $this->announce->id,
+            ]);
+        }
+
+        $this->isFavorited = !$this->isFavorited;
     }
 
     public function closeDeleteModal()
